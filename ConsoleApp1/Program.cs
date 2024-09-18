dc81c45a4efd48bff10cdddbc69fff7cac7cb3eb
4960017212 <4960017212@NT0887058MT1580>
2024-09-18 15:12:10 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+{
+    Console.WriteLine("Digite o primeiro número: ");
+    int numero1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Digite o segundo número:");
+    int numero2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Selecione a operação");
+    Console.WriteLine("1 - Adição");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Multiplicação");
+    Console.WriteLine("1 - Divisão");
+
+    int operacao = Convert.ToInt32(Console.ReadLine());
+    int resultado = 0;
+
+    switch (operacao)
+    {
+        case 1:
+            resultado = numero1 + numero2;
+            break;
+        case 2:
+            resultado = numero1 - numero2;
+            break;
+        case 3:
+            resultado = numero1 * numero2;
+            break;
+        case 4:
+            resultado = numero1 / numero2;
+            break;
+        default:
+            Console.WriteLine("Operação inválida");
+            break;
+    }   
+    Console.WriteLine("O resultado da operação é: " + resultado);
+}
